ae04ee0e1d7ba823dbedb4cec37c2257e6159d0b
Tomasz Heflik <tomasz.heflik@sap.com>
2016-08-22 09:19:11 +0200

    Implement generate SSH key pair

--- simple_api_server/main.go
+++ simple_api_server/main.go
@@ -1,31 +1,70 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"net/http"
 )
 
 type Message struct {
-	Text string
+	Priv,Pub string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Simple REST service (ssh keys, deploy.jsom). Hello %s!", r.URL.Path[1:])
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
-	m := Message{"Welcome in my first ever API server"}
+func sshkeys(w http.ResponseWriter, r *http.Request) {
+	// genetare private Key
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Println(err.Error)
+		return
+	}
+	err = priv.Validate()
+	if err != nil {
+		fmt.Println("Validation priv failed", err)
+	}
+	priv_def := x509.MarshalPKCS1PrivateKey(priv)
+
+	priv_blk := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   priv_def,
+	}
+	priv_pem := string(pem.EncodeToMemory(&priv_blk))
+
+	// generate public key
+	pub := priv.PublicKey
+	pub_def, err := x509.MarshalPKIXPublicKey(&pub)
+	if err != nil {
+		fmt.Println(err.Error)
+		return
+	}
+	pub_blk := pem.Block{
+		Type:    "PUBLIC KEY",
+		Headers: nil,
+		Bytes:   pub_def,
+	}
+	pub_pem := string(pem.EncodeToMemory(&pub_blk))
+	fmt.Println(priv_pem)
+	fmt.Println(pub_pem)
+
+	// Write to browser
+	m := Message{priv_pem,pub_pem}
 	b, err := json.Marshal(m)
 	if err != nil {
 		panic(err)
 	}
-
 	w.Write(b)
 }
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/about/", about)
+	http.HandleFunc("/ssh/keys/", sshkeys)
 	http.ListenAndServe(":8081", nil)
 
 }
